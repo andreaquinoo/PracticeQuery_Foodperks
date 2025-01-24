202d81535f1ac75b690bf6ed0c96d5be63e3af6e
Andre Aquino <andre.aquino@foodpanda.ph>
2025-01-24 16:12:42 +0800

    revised sql practice query

--- practicequery.sql
+++ practicequery.sql
@@ -12,19 +12,20 @@ voucher value
 voucher code used */
 
 SELECT
-  created_date_local AS OrderDate,
+  created_date_local AS order_date,
   rider_dropped_off_at_local AS delivery_date,
-  order_code AS order_code,
-  vendor_name AS vendor_name,
-  order_status AS order_status,
-  payment_method AS payment_method,
+  order_code,
+  vendor_name,
+  order_status,
+  payment_method,
   gfv_local AS food_value,
   discount_value_local AS discount_value,
-  discount_title AS discount_title,
-   voucher_value_local AS voucher_value,
+  discount_title,
+  voucher_value_local AS voucher_value,
   voucher_code AS voucher_code_used
 FROM `fulfillment-dwh-production.pandata_datamart.pandora__agg_orders`
-WHERE customer_code = 'pkt5e9pb'
+WHERE global_entity_id = 'FP_PH'
+  AND customer_code = "phygkmaa" -- or "phaof4ka" (personal account)
   AND is_successful
   AND DATE(created_date_local) >= DATE_SUB(CURRENT_DATE(), INTERVAL 12 MONTH)
 ORDER BY 
